942fa48f56b9efe0fccd968e917d4c7dd9325574
husky <1101648204@qq.com>
2019-09-30 15:52:45 +0800

    Redission分布式锁

---
+++ provider/src/main/java/com/zzx/provider/redisson/SellController.java
@@ -0,0 +1,44 @@
+package com.zzx.provider.redisson;
+
+
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class SellController {
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private RedissonClient redisson;
+
+    @GetMapping("/save")
+    public void add(){
+        redisTemplate.delete("zzx");
+        redisTemplate.opsForValue().set("zzx","100");
+    }
+
+    private final String LOCKKEY = "locks";
+
+    @GetMapping("/sell")
+    public void sell(){
+        for(int i=0; i < 55; i++){
+            RLock lock = redisson.getLock(LOCKKEY);
+            lock.lock(60, TimeUnit.SECONDS); //加锁，60秒后自动释放锁 （默认30秒）
+            int stock = Integer.parseInt(String.valueOf(redisTemplate.opsForValue().get("zzx")));
+            if(stock > 0){
+                redisTemplate.opsForValue().set("zzx",(stock-1)+"");
+                System.out.println(LOCKKEY+":"+(stock-1)+"");
+            }
+            lock.unlock(); //释放锁
+        }
+    }
+}
