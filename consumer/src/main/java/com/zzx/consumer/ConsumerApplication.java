63dbcdcf6da45df69a850efea3f8967996a47d59
husky <1101648204@qq.com>
2019-09-30 15:50:38 +0800

    Redission分布式锁

--- consumer/src/main/java/com/zzx/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/zzx/consumer/ConsumerApplication.java
@@ -1,7 +1,13 @@
 package com.zzx.consumer;
 
+import io.lettuce.core.dynamic.annotation.Value;
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +18,32 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @Configuration
 @EnableFeignClients
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
 public class ConsumerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class, args);
+    private String host;
+    private String port;
+    private String password;
+
+    @Bean
+    public RedissonClient getRedisson(){
+
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port).setPassword(password).setClientName("locks");
+        //添加主从配置
+        //config.useMasterSlaveServers().setMasterAddress("").setPassword("").addSlaveAddress(new String[]{"",""});
+        return Redisson.create(config);
     }
+
     @Bean
     public RestTemplate template(){
         return new RestTemplate();
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
 
 }
